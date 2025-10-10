aa724efb191144db06aeacb44ec6cd85371d0c1b
Saif-Ahmed-N <saifahmedn2004@gmail.com>
2025-10-10 13:20:57 +0530

    Added to main website

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ app.add_middleware(
         "https://vingsfire-chatbot.netlify.app", # Your production frontend
         "http://127.0.0.1:5500", # Local testing
         "http://localhost:5500",
-        "https://vingsfire-chatbot-prod.vercel.app"  # Local testing
+        "https://vingsfire-chatbot-prod.vercel.app",  # Local testing
+        "https://infinitetechai.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
